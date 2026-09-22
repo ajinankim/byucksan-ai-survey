951c3bcbb49b261c4e65f5789733966edbda10e2
ajinankim <a.jinan.kim@gmail.com>
2026-09-22 13:12:13 +0900

    Config no-store + init auto-retry 3x (fix '문항을 불러오지 못했습니다')

--- api/config.js
+++ api/config.js
@@ -1,6 +1,7 @@
 import { QUESTIONS, COMMENT } from "./_questions.js";
 
 export default function handler(req, res) {
-  res.setHeader("Cache-Control", "public, max-age=60");
+  // 문항은 수정 가능성이 있으므로 캐시 금지 (60초 캐시가 배포 직후 stale 구조 재사용 문제 유발)
+  res.setHeader("Cache-Control", "no-store");
   res.status(200).json({ questions: QUESTIONS, comment: COMMENT });
 }
